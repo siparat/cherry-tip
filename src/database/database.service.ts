9436bca6b08cdb6a1e96ff767b47bdbc23f72bfb
siparat <akimov1712@yandex.ru>
2024-04-12 14:35:41 +0300

    feat(app): Added database module on prisma

---
+++ src/database/database.service.ts
@@ -0,0 +1,13 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class DatabaseService extends PrismaClient implements OnModuleDestroy, OnModuleInit {
+	async onModuleInit(): Promise<void> {
+		await this.$connect();
+	}
+
+	async onModuleDestroy(): Promise<void> {
+		await this.$disconnect();
+	}
+}
