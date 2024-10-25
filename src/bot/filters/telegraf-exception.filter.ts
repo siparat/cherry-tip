645373632a9ceb62cdd747e8eebc03c7bbb84ec0
siparat <akimov1712@yandex.ru>
2024-10-25 14:23:02 +0300

    fix(app): Updated TelegrafExceptionFilter

--- src/bot/filters/telegraf-exception.filter.ts
+++ src/bot/filters/telegraf-exception.filter.ts
@@ -1,18 +1,24 @@
-import { ArgumentsHost, Catch, ExceptionFilter, Logger } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, Logger } from '@nestjs/common';
 import { Context, ITelegrafError } from '../bot.interface';
 import { TelegrafArgumentsHost } from 'nestjs-telegraf';
 
 @Catch()
 export class TelegrafExceptionFilter implements ExceptionFilter {
-	async catch(exception: ITelegrafError | Error, host: ArgumentsHost): Promise<unknown> {
+	async catch(exception: ITelegrafError | HttpException, host: ArgumentsHost): Promise<unknown> {
 		const telegrafHost = TelegrafArgumentsHost.create(host);
 		const ctx = telegrafHost.getContext<Context>();
 
-		if (typeof exception !== 'object' || !('type' in exception)) {
+		if (typeof exception !== 'object' || (!('type' in exception) && !('getResponse' in exception))) {
 			Logger.error(exception, 'TelegrafException');
-			await ctx.reply('*⛔️ Произошла ошибка на сервере, попробуйте позже ⛔️❗️*', { parse_mode: 'Markdown' });
+			await ctx.reply('*⛔️ Произошла ошибка на сервере, попробуйте позже ⛔️*', { parse_mode: 'Markdown' });
 			return;
 		}
+
+		if (!('type' in exception)) {
+			await ctx.reply(`*⛔️ ${exception.getResponse()['ru']} ⛔️*`, { parse_mode: 'Markdown' });
+			return;
+		}
+
 		switch (exception.type) {
 			case 'error': {
 				await ctx.reply(`*❗️ ${exception.message} ❗️*`, { parse_mode: 'Markdown' });
